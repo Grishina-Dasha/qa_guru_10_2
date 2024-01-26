57ee8d6de56385cfebcb5dc790c8b64808169255
Dasha <d.s.grishina98@gmail.com>
2024-01-26 20:24:52 +0300

    Внесла правки

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.open()
     browser.config.window_height = 1440
     browser.config.window_width = 700
     yield
--- test_new.py
+++ test_new.py
@@ -2,10 +2,12 @@ from selene import have, browser
 
 
 def test_search_1():
+    browser.open("https://www.google.com/")
     browser.element('[name="q"]').type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
 def test_search_2():
+    browser.open("https://www.google.com/")
     browser.element('[name="q"]').type('fghfghdjkghkdujh').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
